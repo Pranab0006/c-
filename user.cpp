9eae1d24d2e69b861ba7488b8084684641876f99
Pranab0006 <158081782+Pranab0006@users.noreply.github.com>
2024-01-29 22:49:49 +0530

    User

---
+++ user.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+string name;
+cout<<"what is your name";
+cin>>name;
+cout<<"your name is: "<<name<<endl;
+
+
+char gender;
+cout<<"what is your gender";
+cin>>gender;
+cout<<"your gender is: "<<gender<<endl;
+
+
+int age;
+cout<<"what is your age";
+cin>>age;
+cout<<"your age is: "<<age<<endl;
+
+
+float weight;
+cout<<"what is your weight";
+cin>>weight;
+cout<<"your weight is: "<<weight<<endl;
+
+
+return 0;
+}
